f4168924fd690651cd1b9171b6ac0d89762b18b5
shivang <shivang@enefits.co>
2023-02-01 21:35:46 +0530

    adding comments for plugin methods.

--- lib/adspostx_flutter.dart
+++ lib/adspostx_flutter.dart
@@ -1,26 +1,62 @@
 import 'adspostx_flutter_platform_interface.dart';
 
 class AdspostxFlutter {
+  /// setEnvironment sets environment for serving offers.
+  /// 'environment' must be either 0 or 1.
+  /// 0 - live environment.
+  /// Default is set to 'live environment'.
+  /// 1 - test environment.
+  /// Returns a bool indicating that if setEnvironment call is successful or not.
+  /// In case of error it can also throw exceptions.  ///
   Future<bool> setEnvironment(int environment) {
     return AdspostxFlutterPlatform.instance.setEnvironment(environment);
   }
 
+  /// It makes debug logs enable/disable.
+  /// By default debug logs are disabled.
+  /// We don't recommand to enable logs in live environment.
+  /// 'isEnabled' must be either true/false.
+  /// true - enable debug log.
+  /// false - disable debug log.
+  /// Returns a bool indicating that if enableDebugLog call is successful or not.
+  /// In case of error it can also throw exceptions.  ///
   Future<bool> enableDebugLog(bool isEnabled) {
     return AdspostxFlutterPlatform.instance.enableDebugLog(isEnabled);
   }
 
+  /// setTimeOut sets timeout value in seconds.
+  /// Used it rarely only when there some problem with offers after loading them.
+  /// Returns a bool indicating that if setTimeout call is successful or not.
+  /// In case of error it can also throw exceptions.  ///
   Future<bool> setTimeout(double seconds) {
     return AdspostxFlutterPlatform.instance.setTimeout(seconds);
   }
 
+  /// Initialize adspostx with accountId, accountId should be non-empty valid String.
+  /// Returns a bool indicating that if init call is successful or not.
+  /// In case of error it can also throw exceptions.
   Future<bool> init(String accountId) {
     return AdspostxFlutterPlatform.instance.init(accountId);
   }
 
+  /// LoadOffers with specified attributes, attributes should be of Map type.
+  /// attribute key-value should be from specific values which you can get from
+  /// adspostx documentation website [https://docs.adspostx.com/].
+  /// Returns a bool indicating that if loadOffers call is successful or not.
+  /// In case of error it can also throw exceptions.
   Future<bool> loadOffers(Map attributes) {
     return AdspostxFlutterPlatform.instance.loadOffers(attributes);
   }
 
+  /// ShowOffers with specified configurations.
+  /// presentationStyle must be either '0' or '1'. 0 - pop up style, 1 - full screen size.
+  /// isTransparent must be either 'true' or 'false' indicates if offer background should be transparent or not.
+  /// topMagin must be of int type and value from 0 to 15. it indicates top margin offset in percentage.
+  /// rightMagin must be of int type and value from 0 to 15. it indicates right margin offset in percentage.
+  /// bottomMargin must be of int type and value from 0 to 15. it indicates bottom margin offset in percentage.
+  /// leftMargin must be of int type and value from 0 to 15. it indicates left margin offset in percentage.
+  /// Returns a bool indicating that if showOffers call is successful or not.
+  /// In case of error it can also throw exceptions.
   Future<bool> showOffers(int presentationStyle, bool isTransparent,
       int topMargin, int rightMargin, int bottomMargin, int leftMargin) {
     return AdspostxFlutterPlatform.instance.showOffers(
--- lib/adspostx_flutter_platform_interface.dart
+++ lib/adspostx_flutter_platform_interface.dart
@@ -23,26 +23,62 @@ abstract class AdspostxFlutterPlatform extends PlatformInterface {
     _instance = instance;
   }
 
+  /// It makes debug logs enable/disable.
+  /// By default debug logs are disabled.
+  /// We don't recommand to enable logs in live environment.
+  /// 'isEnabled' must be either true/false.
+  /// true - enable debug log.
+  /// false - disable debug log.
+  /// Returns a bool indicating that if enableDebugLog call is successful or not.
+  /// In case of error it can also throw exceptions.  ///
   Future<bool> enableDebugLog(bool isEnabled) async {
     throw UnimplementedError('enableDebugLog() has not been implemented.');
   }
 
+  /// setEnvironment sets environment for serving offers.
+  /// 'environment' must be either 0 or 1.
+  /// 0 - live environment.
+  /// Default is set to 'live environment'.
+  /// 1 - test environment.
+  /// Returns a bool indicating that if setEnvironment call is successful or not.
+  /// In case of error it can also throw exceptions.  ///
   Future<bool> setEnvironment(int environment) async {
     throw UnimplementedError('setEnvironment() has not been implemented.');
   }
 
+  /// setTimeOut sets timeout value in seconds.
+  /// Used it rarely only when there some problem with offers after loading them.
+  /// Returns a bool indicating that if setTimeout call is successful or not.
+  /// In case of error it can also throw exceptions.  ///
   Future<bool> setTimeout(double seconds) async {
     throw UnimplementedError('setTimeout() has not been implemented.');
   }
 
+  /// Initialize adspostx with accountId, accountId should be non-empty valid String.
+  /// Returns a bool indicating that if init call is successful or not.
+  /// In case of error it can also throw exceptions.
   Future<bool> init(String accountId) async {
     throw UnimplementedError('init() has not been implemented.');
   }
 
+  /// LoadOffers with specified attributes, attributes should be of Map type.
+  /// attribute key-value should be from specific values which you can get from
+  /// adspostx documentation website [https://docs.adspostx.com/].
+  /// Returns a bool indicating that if loadOffers call is successful or not.
+  /// In case of error it can also throw exceptions.
   Future<bool> loadOffers(Map attributes) async {
     throw UnimplementedError('loadOffers() has not been implemented.');
   }
 
+  /// ShowOffers with specified configurations.
+  /// presentationStyle must be either '0' or '1'. 0 - pop up style, 1 - full screen size.
+  /// isTransparent must be either 'true' or 'false' indicates if offer background should be transparent or not.
+  /// topMagin must be of int type and value from 0 to 15. it indicates top margin offset in percentage.
+  /// rightMagin must be of int type and value from 0 to 15. it indicates right margin offset in percentage.
+  /// bottomMargin must be of int type and value from 0 to 15. it indicates bottom margin offset in percentage.
+  /// leftMargin must be of int type and value from 0 to 15. it indicates left margin offset in percentage.
+  /// Returns a bool indicating that if showOffers call is successful or not.
+  /// In case of error it can also throw exceptions.
   Future<bool> showOffers(
     int presentationStyle,
     bool isTransparent,
